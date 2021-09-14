b96af4f9002872bbb35554ab1f705e18da4fee4b
Eliton <eliton@sisplansistemas.com.br>
2021-09-14 13:10:48 -0300

    finished class 05 of chapter 4

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,30 +12,53 @@ import {
   Tbody,
   Td,
   Text,
+  Spinner,
   useBreakpointValue,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { RiAddLine, RiEditLine } from "react-icons/ri";
+import { useQuery } from "react-query";
 
 import Header from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/SideBar";
+
 export default function UserList() {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://127.0.0.1:3000/api/users");
+    const data = await response.json();
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    });
+
+    return users;
+
+
+    // return data;
+  });
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
 
-  const [userList, setUserList] = useState([]);
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:3000/api/users")
-      .then((response) => response.json())
-      .then((data) => {
-        setUserList(data.users);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://127.0.0.1:3000/api/users")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setUserList(data.users);
+  //     });
+  // }, []);
 
   return (
     <Box>
@@ -59,50 +82,66 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de Cadastro</Th>}
-                <Th width="8">Ações</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {userList.length >0 && userList.map((user) => {
-                return (
-                  <Tr key={user.id}>
-                    <Td px={["4", "4", "6"]}>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
                       <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">{user.name}</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          {user.email}
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td px={["4", "4", "6"]}>{Intl.DateTimeFormat().format(new Date(user.createdAt))}</Td>}
-                    <Td px={["4", "4", "6"]}>
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                        leftIcon={<Icon fontSize="16" as={RiEditLine} />}
-                      >
-                        {isWideVersion && "Editar"}
-                      </Button>
-                    </Td>
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de Cadastro</Th>}
+                    <Th width="8">Ações</Th>
                   </Tr>
-                )
-              })}
-            </Tbody>
-          </Table>
-          <Pagination />
+                </Thead>
+                <Tbody>
+                  {data.map((user) => {
+                      return (
+                        <Tr key={user.id}>
+                          <Td px={["4", "4", "6"]}>
+                            <Checkbox colorScheme="pink" />
+                          </Td>
+                          <Td>
+                            <Box>
+                              <Text fontWeight="bold">{user.name}</Text>
+                              <Text fontSize="sm" color="gray.300">
+                                {user.email}
+                              </Text>
+                            </Box>
+                          </Td>
+                          {isWideVersion && (
+                            <Td px={["4", "4", "6"]}>
+                              {user.createdAt}
+                            </Td>
+                          )}
+                          <Td px={["4", "4", "6"]}>
+                            <Button
+                              as="a"
+                              size="sm"
+                              fontSize="sm"
+                              colorScheme="purple"
+                              leftIcon={<Icon fontSize="16" as={RiEditLine} />}
+                            >
+                              {isWideVersion && "Editar"}
+                            </Button>
+                          </Td>
+                        </Tr>
+                      );
+                    })}
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
